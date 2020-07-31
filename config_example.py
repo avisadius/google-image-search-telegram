551bce807a40f75b505777894b5cbd94dd703000
avisadius <44080415+avisadius@users.noreply.github.com>
2020-08-01 02:48:15 +0700

    Update config_example.py

--- config_example.py
+++ config_example.py
@@ -1,10 +1,10 @@
 import os
 
-BOT_NAME = os.environ.get("BOT_NAME", "")
-CSE_KEY = os.environ.get("CSE_KEY", "")
-CSE_CX = os.environ.get("CSE_CX", "")
-API_TOKEN = os.environ.get("API_TOKEN", "")
-WEBHOOK_URL = os.environ.get("WEBHOOK_URL", "")
+BOT_NAME = os.environ.get("avisadius_bot", "")
+CSE_KEY = os.environ.get("AIzaSyDtASgnIRAWYZIoxefihuH2rgriJ1nFErM", "")
+CSE_CX = os.environ.get("013922059191334204857:gr8msb3oyls", "")
+API_TOKEN = os.environ.get("1370044208:AAHpGzvGzxcJLJ6cF87w6RyQuYL3mllQfWo", "")
+WEBHOOK_URL = os.environ.get("https://avisadius.herokuapp.com", "")
 NGINX_SUBPATH = os.environ.get("NGINX_SUBPATH", "")
 BATCH = int(os.environ.get("BATCH", 10))
 POLLING = bool(os.environ.get("POLLING", False))
